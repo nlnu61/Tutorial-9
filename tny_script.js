2e247a8ceb750f6adc3213e13ace013ce8ab312f
nlnu61 <navdeepkaur.lnu9@rdpolytech.ca>
2024-10-09 11:37:29 -0600

    update

--- tny_script.js
+++ tny_script.js
@@ -10,7 +10,19 @@
    Date: 2024-10-08 
 
 */
+/* Display date & time */
+document.getElementById("dateNow").innerHTML=`m/d/y/ <br />h:m:s`;
+/*Display the time left until New Years Eve */
 
-window.alert("Welcome to Tulsa");
+document.getElementById("days").textcontent="dd";
+document.getElementById("hrs").textcontent="hh";
+document.getElementById("mins").textcontent="mm";
+document.getElementById("secs").textcontent="ss";
 
-//camelCase
+/* store the current date & time */
+var currentDay = new Date("May, 23, 2018 14:35:05");
+var datestr = currentDay.toLocalDateString();
+var timestr = currentDay.toLocalTimeString();
+
+/*Display date & time */
+document.getElementById("dateNow").innerHTML = dateStr + "<br />" timeStr;
