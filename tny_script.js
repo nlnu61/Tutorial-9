ea6003c05be21b92b25e05b43da1157db55b5772
nlnu61 <navdeepkaur.lnu9@rdpolytech.ca>
2024-10-10 11:07:33 -0600

    update

--- tny_script.js
+++ tny_script.js
@@ -12,17 +12,36 @@
 */
 /* Display date & time */
 document.getElementById("dateNow").innerHTML=`m/d/y/ <br />h:m:s`;
-/*Display the time left until New Years Eve */
 
-document.getElementById("days").textcontent="dd";
-document.getElementById("hrs").textcontent="hh";
-document.getElementById("mins").textcontent="mm";
-document.getElementById("secs").textcontent="ss";
 
 /* store the current date & time */
-var currentDay = new Date("May, 23, 2018 14:35:05");
+var currentDay = new Date("October, 10, 2024 14:35:05");
 var datestr = currentDay.toLocalDateString();
 var timestr = currentDay.toLocalTimeString();
 
 /*Display date & time */
-document.getElementById("dateNow").innerHTML = dateStr + "<br />" timeStr;
+document.getElementById("dateNow").innerHTML =
+ dateStr + "<br />" timeStr;
+
+/* Display days left before  January 1st, 2018 */ 
+ var newYear = new Date("January 1, 2024");
+ var nextYear = currentDay.getFullYear() + 1;
+ newYear.setFullYear(nextYear);
+
+ /* Calculate the days left in the current day */ 
+var daysLeft = (newYear - currentDay) / (1000 * 60 * 60 * 24);
+
+ /* Calculate the hours left in the current day */ 
+var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+
+document.getElementById("dateNow").innerHTML=`m/d/y/ <br />h:m:s`;
+
+ /*Display the time left until New Years Eve */
+
+document.getElementById("days").textcontent=Math.floor(daysLeft);
+document.getElementById("hrs").textcontent="hh";
+document.getElementById("mins").textcontent="mm";
+document.getElementById("secs").textcontent="ss";
+
+
+
